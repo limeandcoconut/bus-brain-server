19332e3cffe27b262f6ee46eaaf0547f2c7523c6
Jacob Smith <js@jacobsmith.tech>
2020-03-14 23:20:17 -0700

    Clean up

--- index.js
+++ index.js
@@ -1,33 +1,25 @@
-// const fastify = require('fastify')({ logger: false })
+const express = require('express')
+const cors = require('cors')
 const { isProd, gotMock } = require('./utils')
 const got = isProd() ? require('got') : gotMock
 const parseClients = require('./parse-clients')
 const { port, configFile } = require('./config.js')
 
+const app = express()
 const clients = parseClients(configFile)
 
-// fastify.register(require('fastify-formbody'))
-// fastify.register(require('fastify-cors'), {
-//   methods: ['GET', 'POST'],
-//   origin: [
-//     webPanelHostRegex,
-//     ...Object.values(clients),
-//   ],
-// })
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const app = express()
 app.use(cors({
   methods: ['GET', 'POST'],
   origin: [
     /:\/\/das-mechabus\.jacobsmith\.tech/,
+    /:\/\/localhost:/,
     // These are the local machines
     /:\/\/10.0.0.[2-4]/,
     ...Object.values(clients),
   ],
 }))
-app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(express.json())
 
 app.post('/', async (request, response) => {
   const { id, state, action } = request.body
@@ -37,7 +29,6 @@ app.post('/', async (request, response) => {
     response.set(404)
     return response.send({ Error: 'Controller not found' })
   }
-
   if (action === 'toggle') {
     const reply = await got(`http://${client}/?action=${action}`)
     console.log(reply)
@@ -53,8 +44,3 @@ app.post('/', async (request, response) => {
 })
 
 app.listen(port, () => console.log(`Listening on: ${port}`))
-
-// ;(async () => {
-//   await app.listen(port)
-//   console.log(`server listening on ${fastify.server.address().port}`)
-// })()
