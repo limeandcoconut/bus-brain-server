5c1b3e8fd8fdbdc387edd286db151bdd94fe5f42
Jacob Smith <js@jacobsmith.tech>
2020-03-14 17:09:59 -0700

    Try express

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fastify = require('fastify')({ logger: false })
+// const fastify = require('fastify')({ logger: false })
 const { isProd, gotMock } = require('./utils')
 const got = isProd() ? require('got') : gotMock
 const parseClients = require('./parse-clients')
@@ -6,39 +6,53 @@ const { port, configFile, webPanelHostRegex } = require('./config.js')
 
 const clients = parseClients(configFile)
 
-fastify.register(require('fastify-formbody'))
-fastify.register(require('fastify-cors'), {
+// fastify.register(require('fastify-formbody'))
+// fastify.register(require('fastify-cors'), {
+//   methods: ['GET', 'POST'],
+//   origin: [
+//     webPanelHostRegex,
+//     ...Object.values(clients),
+//   ],
+// })
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+app.use(cors({
   methods: ['GET', 'POST'],
   origin: [
     webPanelHostRegex,
     ...Object.values(clients),
   ],
-})
+}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-fastify.post('/', async (request, response) => {
-  const { id, state, action } = JSON.parse(request.body)
+app.post('/', async (request, response) => {
+  const { id, state, action } = request.body
   const client = clients[id]
 
   if (!client) {
-    response.code(404)
-    response.send({ Error: 'Controller not found' })
+    response.set(404)
+    return response.send({ Error: 'Controller not found' })
   }
 
   if (action === 'toggle') {
     const reply = await got(`http://${client}/?action=${action}`)
     console.log(reply)
-    response.send(reply)
+    return response.send(reply)
   }
   if (typeof state === 'number') {
     const reply = await got(`http://${client}/?state=${state}`)
     console.log(reply)
-    response.send(reply)
+    return response.send(reply)
   }
-  response.code(400)
-  response.send({ Error: 'Invalid request' })
+  response.set(400)
+  return response.send({ Error: 'Invalid request' })
 })
 
-;(async () => {
-  await fastify.listen(port)
-  console.log(`server listening on ${fastify.server.address().port}`)
-})()
+app.listen(port, () => console.log(`Listening on: ${port}`))
+
+// ;(async () => {
+//   await app.listen(port)
+//   console.log(`server listening on ${fastify.server.address().port}`)
+// })()
